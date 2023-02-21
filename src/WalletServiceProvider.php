6d1b4aca325fcd4fc0bf0c2511909d2bab0c4848
d3cr33 <d3cr33@d3cr33s-MacBook-Air.local>
2023-02-21 21:47:44 +0330

    add: add WalletServiceProvider

---
+++ src/WalletServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace D3cr33\Wallet;
+
+use Illuminate\Support\ServiceProvider;
+
+class WalletServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
