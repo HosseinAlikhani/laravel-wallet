8f0b28d08fd454bc80e6a369aa26f2b81be6e40c
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-02-23 16:29:21 +0330

    update: add setUuid method contract

--- src/Events/Wallet/contracts/WalletEventInterface.php
+++ src/Events/Wallet/contracts/WalletEventInterface.php
@@ -3,5 +3,10 @@ namespace D3cr33\Wallet\Events\Wallet\contracts;
 
 interface WalletEventInterface
 {
-
+    /**
+     * set uuid
+     * @param string $uuid
+     * @return WalletEventInterface
+     */
+    public function setUuid(string $uuid): WalletEventInterface;
 }
