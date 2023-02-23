9478f128f38b5289fb4fa478c9daf11374a5d8e7
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-02-23 16:24:55 +0330

    update: add base property to walletEvent

--- src/Events/Wallet/WalletEvent.php
+++ src/Events/Wallet/WalletEvent.php
@@ -3,5 +3,39 @@ namespace D3cr33\Wallet\Events\Wallet;
 
 class WalletEvent
 {
+    /**
+     * event uuid - unique id
+     * @var string
+     */
+    private string $uuid;
 
+    /**
+     * event raise on which user authenticate id
+     * @var string
+     */
+    private string $userId;
+
+    /**
+     * amount of event
+     * @var int
+     */
+    private int $amount;
+
+    /**
+     * balance of event
+     * @var int
+     */
+    private int $balance;
+
+    /**
+     * count of event that applied to aggregate
+     * @var int
+     */
+    private int $eventCount;
+
+    /**
+     * dateTime when event raised
+     * @var string
+     */
+    private string $createdAt;
 }
