df907cd686724e51eb77ea402023de12b0751055
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-02-23 20:51:25 +0330

    update: add apply method

--- src/Wallet.php
+++ src/Wallet.php
@@ -1,7 +1,22 @@
 <?php
 namespace D3cr33\Wallet;
 
+use D3cr33\Wallet\Events\Wallet\contracts\WalletEventInterface;
+
 final class Wallet
 {
+    /**
+     * apply event on wallet
+     * @param WalletEventInterface $walletEvent
+     */
+    public function apply(WalletEventInterface $walletEvent)
+    {
+        $class     = $walletEvent::class;
+        $className = substr(strrchr($class, '\\'), 1);
+        $method    = 'apply' . $className;
+        if (method_exists($this, $method)) {
+            $this->$method($walletEvent);
 
+        }
+    }
 }
