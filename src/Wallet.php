aecbaf2b70e3a351c627d55ccb24378504019c29
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-02-24 16:31:51 +0330

    update: add properties

--- src/Wallet.php
+++ src/Wallet.php
@@ -6,6 +6,42 @@ use D3cr33\Wallet\Events\Wallet\contracts\WalletEventInterface;
 final class Wallet
 {
     /**
+     * event uuid - unique id
+     * @var string
+     */
+    public string $uuid;
+
+    /**
+     * event raise on which user authenticate id
+     * @var string
+     */
+    public string $userId;
+
+    /**
+     * amount of event
+     * @var int
+     */
+    public int $amount;
+
+    /**
+     * balance of event
+     * @var int
+     */
+    public int $balance;
+
+    /**
+     * count of event that applied to aggregate
+     * @var int
+     */
+    public int $eventCount;
+
+    /**
+     * dateTime when event raised
+     * @var string
+     */
+    public string $createdAt;
+
+    /**
      * apply event on wallet
      * @param WalletEventInterface $walletEvent
      */
