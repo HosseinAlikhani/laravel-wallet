dfa6eb0d6fdb7ee93adec69f80b7df922be2cd9c
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-02-24 16:23:26 +0330

    add: add Wallet model + add guarded property

---
+++ src/Models/Wallet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace D3cr33\Wallet\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
