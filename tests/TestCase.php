5b2753c46c12bd1b08deecb0b87a9a7b52673cb8
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-02 01:18:32 +0330

    update: update testCase getEnvironmentSetUp method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,11 +6,6 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function setUp(): void
     {
         parent::setUp();
@@ -23,12 +18,12 @@ class TestCase extends OrchestraTestCase
         // Setup default database to use sqlite :memory:
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
-            'driver' => env('DB_CONNECTION'),
-            'host' =>   env('DB_HOST'),
-            'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'driver' => 'mysql',
+            'host' =>   '127.0.0.1',
+            'port' => '3306',
+            'database' => 'laravel-wallet',
+            'username' => 'root',
+            'password' => '',
         ]);
     }
 
