f2bb88d2dd5e2821c10789ba0689a9fda211ebbc
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-02 01:40:46 +0330

    add: add wallet_events migration

---
+++ database/migrations/2023_02_25_120716_create_wallet_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use D3cr33\Wallet\Events\DecreaseWalletEvent;
+use D3cr33\Wallet\Events\IncreaseWalletEvent;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallet_events', function (Blueprint $table) {
+            $table->uuid('uuid');
+            $table->string('user_id');
+            $table->integer('amount');
+            $table->integer('balance');
+            $table->enum('event_type', [
+                IncreaseWalletEvent::EVENT_TYPE,
+                DecreaseWalletEvent::EVENT_TYPE
+            ]);
+            $table->integer('event_count');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallet_events');
+    }
+};
