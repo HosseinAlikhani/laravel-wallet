8aa30bc2530e60851d3a1faae3073f83eef72bc6
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-04 01:14:44 +0330

    update: add getEventType method

--- src/Core/Events/DecreaseWalletEvent.php
+++ src/Core/Events/DecreaseWalletEvent.php
@@ -4,4 +4,13 @@ namespace D3cr33\Wallet\Core\Events;
 final class DecreaseWalletEvent extends WalletEvent
 {
     public const EVENT_TYPE = 'DecreaseWallet';
+
+    /**
+     * get event type
+     * @return string
+     */
+    public function getEventType(): string
+    {
+        return self::EVENT_TYPE;
+    }
 }
