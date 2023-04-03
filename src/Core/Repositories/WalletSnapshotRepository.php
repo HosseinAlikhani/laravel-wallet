f02d34cc8e53c32beef9159b2e8da5ee7810bb4c
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-03 22:23:52 +0330

    add: add WalletSnapshotRepository with findSnapshotUserId method

---
+++ src/Core/Repositories/WalletSnapshotRepository.php
@@ -0,0 +1,25 @@
+<?php
+namespace D3cr33\Wallet\Core\Repositories;
+
+use D3cr33\Wallet\Core\Wallet;
+use Illuminate\Support\Facades\DB;
+
+final class WalletSnapshotRepository
+{
+    /**
+     * store table name
+     */
+    public const TABLE = 'wallet_snapshots';
+
+    /**
+     * find snapshot by user id
+     * @param int $userId
+     * @return Wallet|null
+     */
+    public function findSnapshotByUserId(int $userId): Wallet|null
+    {
+        $snapshot = DB::table(self::TABLE)
+            ->where('user_id', $userId)->first();
+        return $snapshot ? Wallet::toObject($snapshot) : null;
+    }
+}
