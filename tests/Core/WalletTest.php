38a2ca30027a71e62ddd619ba553a3b9cd9598f8
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-05 01:22:15 +0330

    update: add method test for initialize static method

--- tests/Core/WalletTest.php
+++ tests/Core/WalletTest.php
@@ -1,12 +1,21 @@
 <?php
 namespace D3CR33\Wallet\Test\Core;
 
+use D3cr33\Wallet\Core\Wallet;
 use D3cr33\Wallet\Test\TestCase;
 
 class WalletTest extends TestCase
 {
-    public function test_wallet()
+    public function test_wallet_initialize()
     {
-        dd('ok');
+        $userId = fake()->numberBetween(1000, 9999);
+        $wallet = Wallet::initialize($userId);
+
+        $this->assertNotNull($wallet->uuid);
+        $this->assertEquals($userId, $wallet->userId);
+        $this->assertEquals(0, $wallet->amount);
+        $this->assertEquals(0, $wallet->balance);
+        $this->assertNull($wallet->eventType);
+        $this->assertEquals(0, $wallet->eventCount);
     }
 }
