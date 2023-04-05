f944e6680fab3b15b17fa33a50803e99741c790c
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-06 02:13:46 +0330

    update: add status / message / data property + add constructor

--- src/Response/WalletResponse.php
+++ src/Response/WalletResponse.php
@@ -1,7 +1,44 @@
 <?php
 namespace D3cr33\Wallet\Response;
 
-final class WalletResponse
+use D3cr33\Wallet\Contracts\WalletResponseInterface;
+
+final class WalletResponse implements WalletResponseInterface
 {
-    
+    /**
+     * store status
+     * @var int
+     */
+    private int $status;
+
+    /**
+     * store message
+     * @var string
+     */
+    private string $message;
+
+    /**
+     * store data
+     * @var array
+     */
+    private array $data;
+
+    /**
+     * constructor of wallet response
+     * @param int $status
+     * @param string $message
+     * @param array $data
+     * @param $args
+     */
+    private function __construct(
+        int $status,
+        string $message,
+        array $data,
+        ...$args
+    )
+    {
+        $this->status = $status;
+        $this->message = $message;
+        $this->data = $data;
+    }
 }
