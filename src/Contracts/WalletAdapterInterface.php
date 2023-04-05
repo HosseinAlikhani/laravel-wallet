4867445625a54c5fcd43e7fbe836eb8103e51383
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-06 02:34:46 +0330

    update: add userWallet contract

--- src/Contracts/WalletAdapterInterface.php
+++ src/Contracts/WalletAdapterInterface.php
@@ -3,5 +3,10 @@ namespace D3cr33\Wallet\Contracts;
 
 interface WalletAdapterInterface
 {
-    
+    /**
+     * get user wallet
+     * @param int $userId
+     * @return WalletResponseInterface
+     */
+    public function userWallet(int $userId): WalletResponseInterface;
 }
