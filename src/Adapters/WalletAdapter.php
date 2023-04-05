65a28b821a90752b3556513a451f1bceb6373965
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-06 02:03:35 +0330

    update: add walletRepository property + add constructor with dependency

--- src/Adapters/WalletAdapter.php
+++ src/Adapters/WalletAdapter.php
@@ -1,7 +1,18 @@
 <?php
 namespace D3cr33\Wallet\Adapters;
 
+use D3cr33\Wallet\Core\Repositories\WalletRepository;
+
 final class WalletAdapter implements WalletAdapterInterface
 {
+    /**
+     * store wallet repository
+     * @var WalletRepository
+     */
+    private WalletRepository $walletRepository;
 
+    public function __construct(WalletRepository $walletRepository)
+    {
+        $this->walletRepository = $walletRepository;    
+    }
 }
