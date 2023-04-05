6773148435d26674eb5742ce3e833f3ab9fce13e
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-06 02:35:55 +0330

    update: add new trans message

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -2,4 +2,5 @@
 return [
     'event_count_not_valid' =>  'event count not valid!',
     'wallet_record_not_valid'   =>  'wallet record not valid!',
+    'user_wallet_not_found' =>  'user wallet not found!',
 ];
