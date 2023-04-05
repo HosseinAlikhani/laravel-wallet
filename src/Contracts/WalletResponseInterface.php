3be2a125d5a8aef47646c029e5d60b9382b6ab69
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-04-06 02:15:40 +0330

    update: add getData contract

--- src/Contracts/WalletResponseInterface.php
+++ src/Contracts/WalletResponseInterface.php
@@ -3,5 +3,9 @@ namespace D3cr33\Wallet\Contracts;
 
 interface WalletResponseInterface
 {
-    //
+    /**
+     * get response data
+     * @return array
+     */
+    public function getData(): array;
 }
