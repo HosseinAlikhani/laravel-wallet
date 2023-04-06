a1d0e42dba823328bc72523ba10d07f857f47349
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-06 16:49:19 +0330

    update: add snapshot + userId + amount method

--- tests/WalletFaker.php
+++ tests/WalletFaker.php
@@ -1,7 +1,35 @@
 <?php
 namespace D3cr33\Wallet\Test;
 
+use D3cr33\Wallet\Core\Wallet;
+
 final class WalletFaker
 {
-    
+    /**
+     * generate fake snapshot
+     * @param array $state
+     * @return Wallet
+     */
+    public function snapshot(array $state = []): Wallet
+    {
+        return (Wallet::initialize($this->userId()))->increase($this->amount());
+    }
+
+    /**
+     * generate fake user id
+     * @return string
+     */
+    public function userId(): string
+    {
+        return fake()->uuid();
+    }
+
+    /**
+     * generate amount
+     * @return int
+     */
+    public function amount(): int
+    {
+        return fake()->numerify("###000");
+    }
 }
