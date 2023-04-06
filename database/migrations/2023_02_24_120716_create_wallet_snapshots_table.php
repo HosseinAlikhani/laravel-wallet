67ff584c5e300831446bbb5844d5f1e6c5789a7f
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-06 16:05:31 +0330

    update: update event_type enum

--- database/migrations/2023_02_24_120716_create_wallet_snapshots_table.php
+++ database/migrations/2023_02_24_120716_create_wallet_snapshots_table.php
@@ -2,6 +2,8 @@
 
 use D3cr33\Wallet\Core\Events\DecreaseWalletEvent;
 use D3cr33\Wallet\Core\Events\IncreaseWalletEvent;
+use D3cr33\Wallet\Core\Events\LockWalletEvent;
+use D3cr33\Wallet\Core\Events\UnLockWalletEvent;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,7 +22,9 @@ return new class extends Migration
             $table->integer('balance');
             $table->enum('event_type', [
                 IncreaseWalletEvent::EVENT_TYPE,
-                DecreaseWalletEvent::EVENT_TYPE
+                DecreaseWalletEvent::EVENT_TYPE,
+                LockWalletEvent::EVENT_TYPE,
+                UnLockWalletEvent::EVENT_TYPE
             ]);
             $table->integer('event_count');
             $table->timestamps();
