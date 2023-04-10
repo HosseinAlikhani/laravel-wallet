72c33198b481753ba274d368fdb9d50c9ce2434a
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-10 22:15:44 +0330

    add: add messages

---
+++ lang/fa/messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'event_count_not_valid' =>  'event count not valid!',
+    'wallet_record_not_valid'   =>  'wallet record not valid!',
+    'user_wallet_not_found' =>  'user wallet not found!',
+    'wallet_charge_successfully'    =>  'کیف پول شما با موفقیت شارژ شد.',
+];
