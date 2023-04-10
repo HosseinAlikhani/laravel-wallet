a521c88c7abd7add848588ac3df7e272ccaa2b8f
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-10 22:09:15 +0330

    update: rename class + update userWallet method

--- src/Ports/WalletPort.php
+++ src/Ports/WalletPort.php
@@ -1,14 +1,13 @@
 <?php
-namespace D3cr33\Wallet\Adapters;
+namespace D3cr33\Wallet\Ports;
 
-use D3cr33\Wallet\Contracts\WalletAdapterInterface;
 use D3cr33\Wallet\Contracts\WalletResponseInterface;
 use D3cr33\Wallet\Core\Repositories\WalletRepository;
 use D3cr33\Wallet\Resources\UserWalletResource;
 use D3cr33\Wallet\Response\WalletResponse;
 use Illuminate\Http\Response;
 
-final class WalletAdapter implements WalletAdapterInterface
+final class WalletPort
 {
     /**
      * store wallet repository
@@ -29,17 +28,10 @@ final class WalletAdapter implements WalletAdapterInterface
     public function userWallet(string $userId): WalletResponseInterface
     {
         $snapshot = $this->walletRepository->findSnapshotByUserId($userId);
-        if(! $snapshot){
-            return (new WalletResponse(
-                Response::HTTP_NOT_FOUND,
-                trans(''),
-                null
-            ));
-        }
         return new WalletResponse(
             Response::HTTP_OK,
             null,
-            (new UserWalletResource($snapshot))->response()->getData(true)
+            $snapshot ? (new UserWalletResource($snapshot))->response()->getData(true) : null
         );
     }
 }
