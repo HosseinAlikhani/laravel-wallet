2bbfe121c734e5ab117e6cca11ae5d6d2a8607d8
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-10 22:23:41 +0330

    update: add wallet_debit_successfully

--- lang/fa/messages.php
+++ lang/fa/messages.php
@@ -3,5 +3,6 @@ return [
     'event_count_not_valid' =>  'event count not valid!',
     'wallet_record_not_valid'   =>  'wallet record not valid!',
     'user_wallet_not_found' =>  'user wallet not found!',
-    'wallet_charge_successfully'    =>  'کیف پول شما با موفقیت شارژ شد.',
+    'wallet_charge_successfully'    =>  'کیف پول با موفقیت شارژ شد.',
+    'wallet_debit_successfully' =>  'مبلغ :amount با موفقیت از کیف پول کسر شد.'
 ];
