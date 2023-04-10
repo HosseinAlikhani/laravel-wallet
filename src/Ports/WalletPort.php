e20016cfd1015bf39fff1905681a4c93d41e58cf
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-10 22:15:35 +0330

    update: add walletCharge method

--- src/Ports/WalletPort.php
+++ src/Ports/WalletPort.php
@@ -3,6 +3,7 @@ namespace D3cr33\Wallet\Ports;
 
 use D3cr33\Wallet\Contracts\WalletResponseInterface;
 use D3cr33\Wallet\Core\Repositories\WalletRepository;
+use D3cr33\Wallet\Core\Wallet;
 use D3cr33\Wallet\Resources\UserWalletResource;
 use D3cr33\Wallet\Response\WalletResponse;
 use Illuminate\Http\Response;
@@ -32,6 +33,26 @@ final class WalletPort
             Response::HTTP_OK,
             null,
             $snapshot ? (new UserWalletResource($snapshot))->response()->getData(true) : null
+        );
+    }
+
+    /**
+     * charge user wallet
+     * @param string $userId
+     * @param string $amount
+     * @param string|null $description
+     * @return WalletResponseInterface
+     */
+    public function walletCharge(string $userId, string $amount, string|null $description = null): WalletResponseInterface
+    {
+        $wallet = Wallet::initialize($userId)
+            ->increase($amount,[
+                'description'   =>  $description
+            ]);
+        return new WalletResponse(
+            Response::HTTP_OK,
+            trans('wallet::messages.wallet_charge_successfully'),
+            (new UserWalletResource($wallet))->response()->getData(true)
         );
     }
 }
