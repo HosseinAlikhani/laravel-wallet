5be6c27c0ffda01ab7d64b86125afee9fead805c
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-21 21:25:03 +0330

    update: add test_update_or_create_method_success method test

--- tests/Domain/WalletRepositoryTest.php
+++ tests/Domain/WalletRepositoryTest.php
@@ -2,9 +2,12 @@
 /**
  * test wallet repository
  * 1- test save event method success
+ * 2- test update or create method success
  */
 namespace D3CR33\Wallet\Test\Domain;
 
+use D3cr33\Wallet\Core\Events\IncreaseWalletEvent;
+use D3cr33\Wallet\Core\Wallet;
 use D3cr33\Wallet\Test\TestCase;
 
 class WalletRepositoryTest extends TestCase
@@ -20,5 +23,23 @@ class WalletRepositoryTest extends TestCase
 
         $event = $this->faker->makeWalletRepository()->findEventByUuid($eventInstance->uuid);
         $this->assertEquals( $eventInstance, $event );
+    }
+
+    /**
+     * test update or create method success
+     */
+    public function test_update_or_create_method_success()
+    {
+        $userId = $this->faker->userId();
+        $wallet = Wallet::initialize($userId);
+        $wallet->eventType = IncreaseWalletEvent::EVENT_TYPE;
+        $this->assertTrue(
+            $this->faker->makeWalletRepository()->updateOrCreateSnapshot($wallet)
+        );
+
+        $snapshot = $this->faker->makeWalletRepository()->findSnapshotByUserId( $userId );
+
+        $this->assertInstanceOf(Wallet::class, $snapshot);
+        $this->assertEquals( $wallet, $snapshot );
     }
 }
